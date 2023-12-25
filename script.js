f23c6eb54ca0b5f204d6e9f90cd27d8547ec2816
rahulpanchal0106 <95669983+rahulpanchal0106@users.noreply.github.com>
2023-12-25 10:06:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ socket.on('newuser',(data)=>{
 })
 
 socket.on('userLeft',()=>{
-    const user_left = `<div id="joined">${user_name} Left the chat</div>`
+    const user_left = `<div id="joined">Someone Left the chat</div>`
     document.querySelector('#chat-messages').innerHTML+=user_left;
     console.log(user_left)
     socket.emit('userLeft',user_left);
